210de9f23685d597a05974d2411dbebfe2c35cf4
avinashreddymudireddy <avinash.mudireddy@gmail.com>
2024-11-12 21:29:52 -0600

    First app structure creations

---
+++ config/initializers/cookies_serializer.rb
@@ -0,0 +1,6 @@
+# Be sure to restart your server when you modify this file.
+
+# You can control how Rails serializes cookies by setting the `:serializer` option.
+# Available options are `:json`, `:marshal`, and `:hybrid`.
+
+Rails.application.config.action_dispatch.cookies_serializer = :json
