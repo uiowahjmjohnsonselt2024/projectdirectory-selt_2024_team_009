78cd25afbeec8997afacbc952b132c12ea62ecdd
avinashreddymudireddy <avinash.mudireddy@gmail.com>
2024-11-15 12:53:25 -0600

    environment variables are settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
+  config.secret_key_base = Rails.application.credentials.secret_key_base
+
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
                                        .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
