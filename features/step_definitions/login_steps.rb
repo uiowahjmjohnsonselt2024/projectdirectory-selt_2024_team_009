8e8de4af45aaf6355bc892b9024bb5e3ae99f038
Mike Zeng <mzen17.jp@gmail.com>
2024-11-15 23:33:46 -0600

    TESTS: Cucumber tests for the login/sign up

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,21 @@
+Given('a user with email {string} and password {string}') do |email, password|
+    # Here, you would create a user in your test database or mock setup
+    @user = User.create!(email: email, password: password, username: "email")
+  end
+  
+  When('I go {string}') do |path|
+    visit path
+  end
+  
+  When('I fill {string} with {string}') do |field, value|
+    fill_in field, with: value
+  end
+  
+  When('I press {string}') do |button|
+    click_button button
+  end
+  
+  Then('I should see {string}') do |text|
+    expect(page).to have_content(text)
+  end
+  
