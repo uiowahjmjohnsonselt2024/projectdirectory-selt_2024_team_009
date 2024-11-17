12671ca78806505eca2a64215f8dc1cc6ebcf71b
Jack Ossenkop <jackOsse17@gmail.com>
2024-11-17 14:35:28 -0600

    added the treasure and user spec files

--- spec/models/treasure_spec.rb
+++ spec/models/treasure_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Treasure, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  #similar tests for this are present in the inventory file
+  # I am not sure what would be different
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.create(username: "PlayerOne", email: "player@example.com", password: "password123", score: 100) }
+
+  describe "User attributes" do
+    it "should have a username" do
+      expect(user.username).to eq("PlayerOne")
+    end
+
+    it "should have an email" do
+      expect(user.email).to eq("player@example.com")
+    end
+
+    it "should have a password" do
+      expect(user.password).to eq("password123")
+    end
+
+    it "should have a player score" do
+      expect(user.score).to eq(100)
+    end
+
+    it "should calculate account age correctly" do
+      account_age = (Time.now - user.created_at).to_i / (60 * 60 * 24)  # Convert seconds to days
+      expect(user.account_age).to eq(account_age)
+    end
+  end
+
+  describe "Validations" do
+    it "should validate presence of username" do
+      user.username = nil
+      expect(user).not_to be_valid
+      expect(user.errors[:username]).to include("can't be blank")
+    end
+
+    it "should validate presence of email" do
+      user.email = nil
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "should validate presence of password" do
+      user.password = nil
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "should validate presence of score" do
+      user.score = nil
+      expect(user).not_to be_valid
+      expect(user.errors[:score]).to include("can't be blank")
+    end
+  end
 end
