945b213c3e29688d61b0875a253e9f3f576fe0e0
mmreddy743 <mmreddy7743@gmail.com>
2024-11-27 02:40:47 -0600

    Role Based sign up and update role in profile update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_27_065711) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_27_080557) do
   create_table "contents", force: :cascade do |t|
     t.text "story_text"
     t.string "image_url"
@@ -131,6 +131,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_27_065711) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "role", default: "player", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
