ef394e0ad039355fde26be5dee1c39a5f11a3a9e
avinashreddymudireddy <avinash.mudireddy@gmail.com>
2024-12-10 07:46:01 -0600

    heroku push avinash cable link updates code

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,9 +1,8 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the `bin/rails generate channel` command.
-
 import { createConsumer } from "@rails/actioncable";
 
-const cableURL = process.env.RAILS_ENV === 'production'
+const cableURL = window.location.hostname === 'shards-of-the-grid-team-09.herokuapp.com'
     ? 'wss://shards-of-the-grid-team-09.herokuapp.com/cable'
     : 'ws://localhost:3000/cable';
 
