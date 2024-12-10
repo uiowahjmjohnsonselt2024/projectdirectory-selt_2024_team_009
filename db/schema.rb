9fcea2d2f43a76dab0fd71776f031c242c93aae5
mmreddy743 <mmreddy7743@gmail.com>
2024-12-10 01:07:20 -0600

    deleted unused items in inventory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_09_210349) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_10_070012) do
   create_table "contents", force: :cascade do |t|
     t.text "story_text"
     t.string "image_url"
@@ -114,7 +114,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_09_210349) do
     t.decimal "amount", precision: 10, scale: 2
     t.string "currency"
     t.string "payment_method"
-    t.integer "item_id", null: false
+    t.integer "item_id"
     t.integer "quantity"
     t.text "description"
     t.datetime "created_at", null: false
